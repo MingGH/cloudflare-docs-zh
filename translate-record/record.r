e4952aa61993f87ae847356fa82ce51a82019fdb
MingGH <1558772318@qq.com>
2024-07-13 09:29:51 +0800

    更新翻译前源文件hash，用来判断文件是否已经被翻译过

--- translate-record/record.r
+++ translate-record/record.r
@@ -1,8 +1,8 @@
 /cloudflare-docs-zh/README.md,2099-07-12 23:20:42
 /cloudflare-docs-zh/SNIPPETS.md,2099-07-12 23:34:24
 /cloudflare-docs-zh/CONTRIBUTING.md,2099-07-12 23:34:24
-/cloudflare-docs-zh/content/pages/functions/pricing.md,2024-07-12 23:46:13,0953f82aa01fe1eb3416bbe533c49127c6cf82700bfc0966e76fa564a87541f8
-/cloudflare-docs-zh/content/pages/platform/changelog.md,2024-07-13 09:02:38,1f935a6911400d5ddce7257830f6154740e77a5a48366af9c1aa1fdf64f2a18d
-/cloudflare-docs-zh/content/pages/_index.md,2024-07-13 09:03:29,3e9db696f79797b733b57b394916b6f60656af206d870c9dab29e44a8cb6335c
-/cloudflare-docs-zh/content/pages/how-to/deploy-a-wordpress-site.md,2024-07-13 09:03:35,52a67f147886adf6869b96aa69d4e0d969157abde4186159cb63881a4c555ba6
+/cloudflare-docs-zh/content/pages/functions/pricing.md,2024-07-12 23:46:13,90e91c7a88e35620fa595dd8355b527142b6c8c472fdddfb3b24d57c677486d6
+/cloudflare-docs-zh/content/pages/platform/changelog.md,2024-07-13 09:02:38,5832b73ac7014381392d332c360700853a70137bd714f50e19daf1c614426ea5
+/cloudflare-docs-zh/content/pages/_index.md,2024-07-13 09:03:29,01b30236f77e8d88c29cd53894c42658a4f722e6d423fee4d950e06f267c99c5
+/cloudflare-docs-zh/content/pages/how-to/deploy-a-wordpress-site.md,2024-07-13 09:03:35,81ceba0a135fae0a74a001f461a84ff7dd6200424c09e9f29aed5837269605aa
 /cloudflare-docs-zh/content/pages/configuration/custom-domains.md,2024-07-13 09:17:32,1c0518e30b58a11e25ad627e0022a458ace8a8bafeb23d3246edd2b3c57f559c
